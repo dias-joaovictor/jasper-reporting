ecb304e862b895cb08e7affd809d40af20ddc647
João Victor Dias <kvj1610@hotmail.com>
2023-01-06 11:28:50 +0100

    first commit

---
+++ src/main/java/com/dias/jasperreporting/JasperReportingApplication.java
@@ -0,0 +1,13 @@
+package com.dias.jasperreporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperReportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasperReportingApplication.class, args);
+	}
+
+}
